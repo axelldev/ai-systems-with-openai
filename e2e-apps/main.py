f9b0987bf6d75629d46b598330ab713861d272c9
axelldev <axellsolisrubio@gmail.com>
2026-02-25 13:18:41 -0600

    feat: error handling

--- e2e-apps/main.py
+++ e2e-apps/main.py
@@ -1,15 +1,21 @@
 from dotenv import load_dotenv
-from openai import OpenAI
+from openai import AuthenticationError, OpenAI
 
 load_dotenv()
 
 client = OpenAI()
 
 
-response = client.chat.completions.create(
-    model="gpt-5-mini",
-    messages=[{"role": "user", "content": "Hello, how are you"}],
-    response_format={"type": "json_object"},
-)
+if __name__ == "__main__":
+    try:
+        response = client.chat.completions.create(
+            model="gpt-5-mini",
+            messages=[{"role": "user", "content": "Hello, how are you"}],
+            response_format={"type": "json_object"},
+        )
 
-print(response.choices[0].message.content)
+        print(response.choices[0].message.content)
+    except AuthenticationError:
+        print(
+            "Please double check your authentication key and try again, the one provided is not valid."
+        )
